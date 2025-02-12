24cee502538b31381331a3ac81c6992a5d77904a
Nazneen Akter <nazneenzaman195@gmail.com>
2025-02-12 20:13:31 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function pokemon (names, age) {
+    console.log("hello pokemon" + " " + names + " " + "amar boyos" + " " + age);
+}
+pokemon ("pikachu", 10)
+pokemon ("charmander", 15)
+pokemon ("bulbasaur", 20)
+pokemon ("squirtle", 25)
